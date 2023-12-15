3fc9082e699540cadab3a47fa9b88de08160fd8f
Rohan Karan <rohankaran001@gmail.com>
2023-12-15 18:24:49 +0530

    initial commit

--- lighthouz/schema.py
+++ lighthouz/schema.py
@@ -3,12 +3,13 @@ from marshmallow import fields, Schema
 
 class BenchmarkDetailSchema(Schema):
     class Meta:
-        fields = ("query", "expected_response", "context", "put_type")
+        fields = ("query", "expected_response", "context", "put_type", "token_count")
 
     query = fields.Str(required=True)
     expected_response = fields.Str()
     context = fields.Str()
-    put_type = fields.Str(data_key="type", default="None")
+    put_type = fields.Str()
+    token_count = fields.Int()
 
 
 benchmark_schema = BenchmarkDetailSchema()
