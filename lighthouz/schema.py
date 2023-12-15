72775e88016d9942d1c3765642003356d1b13881
Rohan Karan <rohankaran001@gmail.com>
2023-12-15 18:26:34 +0530

    initial commit

--- lighthouz/schema.py
+++ lighthouz/schema.py
@@ -6,10 +6,10 @@ class BenchmarkDetailSchema(Schema):
         fields = ("query", "expected_response", "context", "put_type", "token_count")
 
     query = fields.Str(required=True)
-    expected_response = fields.Str()
-    context = fields.Str()
-    put_type = fields.Str()
-    token_count = fields.Int()
+    expected_response = fields.Str(allow_none=True)
+    context = fields.Str(allow_none=True)
+    put_type = fields.Str(allow_none=True)
+    token_count = fields.Int(allow_none=True)
 
 
 benchmark_schema = BenchmarkDetailSchema()
