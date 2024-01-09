af337c56998da9d3352b3838a87bc1c28ebb118e
lighthouz ai <srija@MacBook-Air.attlocal.net>
2024-01-09 06:20:33 -0500

    added details to example code; added app.py

--- eval-rag-example.py
+++ eval-rag-example.py
@@ -1,13 +1,6 @@
 import os
-
 import requests
-
-from lighthouz import Lighthouz
-from lighthouz.benchmark import Benchmark
-from lighthouz.evaluation import Evaluation
-
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import PyPDFLoader
@@ -18,23 +11,20 @@ from langchain.llms import HuggingFaceHub, HuggingFaceEndpoint
 from langchain import HuggingFacePipeline
 from langchain.prompts import PromptTemplate
 
-os.environ["OPENAI_API_KEY"] = "sk-xx"
+from lighthouz import Lighthouz
+from lighthouz.benchmark import Benchmark
+from lighthouz.evaluation import Evaluation
 
-def hf_example_function(query: str) -> str:
-    API_URL = (
-        "https://api-inference.huggingface.co/models/meta-llama/Llama-2-7b-chat-hf"
-    )
-    API_KEY = "hf_xx"
-    headers = {f"Authorization": f"Bearer {API_KEY}"}
-    payload = {"inputs": query, }
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()[0]["generated_text"]
+os.environ["OPENAI_API_KEY"] = "sk-xx" # enter your OpenAI key 
+RAG_DOCUMENT = "DATA_FOLDER/FILENAME_TO_GENERATE_RAG_BENCHMARK"
 
-def langchain_llm_model():
+lh = Lighthouz("LH_API_KEY") # To obtain a Lighthouz API key contact srijan@lighthouz.ai
+app_id = "APP_ID_TO_BE_EVALUATED" # App can be created on the dashboard at https://lighthouz.ai/dashboard/. App id can be found here.
+
+def langchain_rag_model():
     print("Initializing LangChain RAG Agent")
 
     # DOCUMENTS_FOLDER = "FOLDER_NAME_OF_DOCUMENTS_TO_BUILD_RAG"
-    DOCUMENTS_FOLDER = "DATA"
     chunk_size = 2000
     chunk_overlap = 150
     collection_name = "data-test_vect_embedding"
@@ -43,11 +33,16 @@ def langchain_llm_model():
     if not os.path.exists(persist_directory) or not os.listdir(persist_directory):
         embeddings = OpenAIEmbeddings()
         documents = []
-        for file in os.listdir(DOCUMENTS_FOLDER):
-            if file.endswith(".pdf"):
-                pdf_path = os.path.join(DOCUMENTS_FOLDER, file)
-                loader = PyPDFLoader(pdf_path)
-                documents.extend(loader.load())
+        if RAG_DOCUMENT.endswith(".pdf"): 
+            pdf_path = os.path.join(RAG_DOCUMENT, file)
+            loader = PyPDFLoader(pdf_path)
+            documents.extend(loader.load())
+        else:
+            for file in os.listdir(RAG_DOCUMENT):
+                if file.endswith(".pdf"):
+                    pdf_path = os.path.join(RAG_DOCUMENT, file)
+                    loader = PyPDFLoader(pdf_path)
+                    documents.extend(loader.load())
         text_splitter = TokenTextSplitter(
             chunk_size=chunk_size, chunk_overlap=chunk_overlap
         )
@@ -76,7 +71,7 @@ def langchain_llm_model():
     retriever = vectDB.as_retriever(return_source_document=True)
 
     # prepare stuff prompt template
-    template = """You are a helpful financial assistant. Your job is to provide the answer for the question based on the given context. 
+    template = """You are a helpful assistant. Your job is to provide the answer for the question based on the given context. 
     ## CONTEXT: {context}
     ## QUESTION: {question}
     ## ANSWER: """.strip()
@@ -101,23 +96,21 @@ def langchain_query_function(query: str) -> str:
     response = rag_model({"query": query})["result"]
     return response
 
-lh = Lighthouz("LH_API_KEY") # To obtain a Lighthouz API key contact srijan@lighthouz.ai
-
 benchmark_generator = Benchmark(lh)
-benchmark_generator.generate_benchmark(file_path="ENTER_FILENAME_TO_GENERATE_RAG_BENCHMARK", benchmarks=["rag_benchmark", "out_of_context", "pii_leak", "prompt_injection"]) # You can also generate benchmarks from https://lighthouz.ai/studio
-
-rag_model = langchain_llm_model()
+benchmark_data = benchmark_generator.generate_benchmark(file_path=RAG_DOCUMENT, benchmarks=["rag_benchmark", "out_of_context", "pii_leak", "prompt_injection"]) 
+benchmark_id = benchmark_data["benchmark_id"]
+rag_model = langchain_rag_model()
 
 evaluation = Evaluation(lh)
 e_single = evaluation.evaluate_rag_model(
     response_function=langchain_query_function,
-    benchmark_id="BENCHMARK_ID", # Enter the benchmark id from above or enter a previous benchmark id. Benchmark ids are on the dashboard https://lighthouz.ai/benchmarks/
-    app_id="APP_ID", # App can be created on the dashboard at https://lighthouz.ai/dashboard/
+    benchmark_id=benchmark_id, # You can also enter an existing benchmark id. Benchmark ids are on the dashboard: https://lighthouz.ai/benchmarks/
+    app_id=app_id, 
 )
 print(e_single)
 
 e_multiple = evaluation.evaluate_multiple_rag_models(
-    response_functions=[langchain_query_function, hf_example_function],
+    response_functions=[langchain_query_function, hf_query_function],
     benchmark_id="BENCHMARK_ID",
     app_ids=["APP_ID1", "APP_ID2"],
 )
