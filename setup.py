ee49fda433ab57bfe3858c08b89126fa75660301
Rohan Karan <rohankaran001@gmail.com>
2024-01-16 17:15:14 +0530

    Change HTTP method in API request from POST to PUT
    In the `lighthouz/app.py` file, the HTTP method used in the API request within the function `register_new_version` has been changed from POST to PUT. This change ensures that the request successfully updates an existing resource rather than creating a new one each time the function is run.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     # Basic info
     name="lighthouz",
-    version="0.1.0",
+    version="0.2.0",
     author="Lighthouz AI, Inc",
     author_email="srijan@lighthouz.ai",
     url="https://github.com/Lighthouz-AI/lighthouz_sdk",
