7cc38dfec6a994ef74577967826211fa78656551
Rohan Karan <rohankaran001@gmail.com>
2024-01-22 16:50:58 +0530

    Update version in setup.py
    The version number for "lighthouz" in setup.py has been updated from "0.2.0" to "0.3.1". This change reflects recent major improvements and updates done to the codebase.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     # Basic info
     name="lighthouz",
-    version="0.2.0",
+    version="0.3.1",
     author="Lighthouz AI, Inc",
     author_email="srijan@lighthouz.ai",
     url="https://github.com/Lighthouz-AI/lighthouz_sdk",
